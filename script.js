11daa95e73570604c4829200f6b814a4c4ae9b83
pieromaduenor <159741285+pieromaduenor@users.noreply.github.com>
2024-02-11 21:20:11 -0500

    script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Gracias por aceptar, te quiero mucho :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
