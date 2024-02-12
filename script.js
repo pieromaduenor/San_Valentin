7916d759fe1aaceba4b380cf8b4f37ad5201c18d
pieromaduenor <159741285+pieromaduenor@users.noreply.github.com>
2024-02-11 21:39:42 -0500

    script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function handleYesClick() {
 function resizeYesButton() {
   const computedStyle = window.getComputedStyle(yesButton);
   const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
+  const newFontSize = fontSize * 0.5;
 
   yesButton.style.fontSize = `${newFontSize}px`;
 }
